e10767694babd91ff73b7f1ef6ea0b7619be5d5a
VIE <anq.viet203@gmail.com>
2024-06-04 14:56:11 +0700

    Done cart and payment screen

--- lib/src/screens/cart_tab.dart
+++ lib/src/screens/cart_tab.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CartTab extends StatelessWidget {
+class CartTab extends StatefulWidget {
   const CartTab({super.key});
 
+  @override
+  State<CartTab> createState() => _CartTabState();
+}
+
+class _CartTabState extends State<CartTab> {
+  var isNoneContact = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,9 +52,139 @@ class CartTab extends StatelessWidget {
                       WidgetStateProperty.all(const Color(0xFF9586A8)),
                   padding: WidgetStateProperty.all(const EdgeInsets.all(0)),
                 ),
-                icon: const Icon(Icons.credit_card),
+                icon: const Icon(
+                  Icons.credit_card,
+                  color: Color(0xFF2D0C57),
+                ),
                 label: const Text('**** **** **** 4747'),
-              )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Delivery address",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2D0C57),
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStateProperty.all(Colors.transparent),
+                      foregroundColor:
+                          WidgetStateProperty.all(const Color(0xFF2D0C57)),
+                    ),
+                    child: const Text('CHANGE'),
+                  )
+                ],
+              ),
+              const Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 6),
+                    child: Icon(Icons.home_outlined, color: Color(0xFF2D0C57)),
+                  ),
+                  Text(
+                    'Alexandra Smith\nCesu 31 k-2 5.st, SIA Chili\nRiga\nLV–1012\nLatvia',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Delivery options",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2D0C57),
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStateProperty.all(Colors.transparent),
+                      foregroundColor:
+                          WidgetStateProperty.all(const Color(0xFF2D0C57)),
+                    ),
+                    child: const Text('CHANGE'),
+                  )
+                ],
+              ),
+              FilledButton.icon(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.transparent),
+                  iconColor: WidgetStateProperty.all(const Color(0xFF000000)),
+                  foregroundColor:
+                      WidgetStateProperty.all(const Color(0xFF9586A8)),
+                  padding: WidgetStateProperty.all(const EdgeInsets.all(0)),
+                ),
+                icon: const Icon(
+                  Icons.directions_walk_outlined,
+                  color: Color(0xFF2D0C57),
+                ),
+                label: const Text('I’ll pick it up myself'),
+              ),
+              FilledButton.icon(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.transparent),
+                  iconColor: WidgetStateProperty.all(const Color(0xFF000000)),
+                  foregroundColor:
+                      WidgetStateProperty.all(const Color(0xFF9586A8)),
+                  padding: WidgetStateProperty.all(const EdgeInsets.all(0)),
+                ),
+                icon: const Icon(
+                  Icons.directions_bike_outlined,
+                  color: Color(0xFF2D0C57),
+                ),
+                label: const Text('By courier'),
+              ),
+              FilledButton.icon(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.transparent),
+                  iconColor: WidgetStateProperty.all(const Color(0xFF000000)),
+                  foregroundColor:
+                      WidgetStateProperty.all(const Color(0xFF9586A8)),
+                  padding: WidgetStateProperty.all(const EdgeInsets.all(0)),
+                ),
+                icon: const Icon(
+                  Icons.airplanemode_active_outlined,
+                  color: Color(0xFF2D0C57),
+                ),
+                label: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('By Drone'),
+                    Icon(
+                      Icons.check,
+                      color: Color(0xFF2D0C57),
+                    ),
+                  ],
+                ),
+              ),
+              SwitchListTile(
+                contentPadding: const EdgeInsets.only(left: 0),
+                title: const Text(
+                  'Delivery options',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2D0C57),
+                  ),
+                ),
+                value: isNoneContact,
+                onChanged: (bool value) => setState(() {
+                  isNoneContact = value;
+                }),
+              ),
             ],
           ),
         ));
--- lib/src/screens/payment_tab.dart
+++ lib/src/screens/payment_tab.dart
@@ -5,10 +5,212 @@ class PaymentTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Payment Tab'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  'Credit / Debit card',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2D0C57),
+                  ),
+                ),
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 374,
+                    height: 240,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Color(0xFFB993D6), Color(0xFF8CA6DB)],
+                        begin: Alignment.topCenter,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  Positioned(
+                    top: -60,
+                    left: 100,
+                    child: Container(
+                      width: 335,
+                      height: 331,
+                      decoration: BoxDecoration(
+                        color: const Color(0x44FFFFFF),
+                        borderRadius: BorderRadius.circular(335),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 32),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Image(
+                          image: AssetImage('assets/images/mc_symbol.png'),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(
+                              top: 25, bottom: 50, right: 12),
+                          child: const Text(
+                            '4747  4747  4747  4747',
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Color(0xFFFFFFFF),
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'alexandra Smith'.toUpperCase(),
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            const Text(
+                              '07/21',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ))
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.camera_alt_outlined),
+                iconSize: 32,
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Name on card'),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF9586A8),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFFD9D0E3),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Card number'),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF9586A8),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFFD9D0E3),
+                          ),
+                        ),
+                        suffixIcon: Icon(Icons.credit_card),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Expiry date'),
+                        InputDatePickerFormField(
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100),
+                          fieldHintText: 'MM/YY',
+                          fieldLabelText: 'MM/YY',
+                          onDateSubmitted: (DateTime value) {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 150,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('CVC'),
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xFF9586A8),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xFFD9D0E3),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20, bottom: 8),
+                child: FilledButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        WidgetStateProperty.all(const Color(0xFF0ACF83)),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'USE THIS CARD',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
